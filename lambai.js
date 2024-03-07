3a01c1ee8a78145ea9f0cb50231eb1f6527cb99f
Đàm Minh Anh <113049898+mahniee03@users.noreply.github.com>
2024-03-07 11:38:49 +0700

    Update lambai.js

--- lambai.js
+++ lambai.js
@@ -1,24 +1,42 @@
-function checkAnswers() {
-    var resultDiv = document.getElementById('result');
-    var correctAnswers = 0;
+let results = [];
 
-    // Lấy đáp án đúng cho mỗi câu hỏi
-    var answers = {
-      "Cau1.1.1": "Cau1.1.1"
-      // Thêm các câu hỏi khác và đáp án đúng ở đây
-    };
-
-    // Kiểm tra đáp án của người dùng và tính số câu trả lời đúng
-    for (var question in answers) {
-      var userAnswer = document.querySelector('input[name="' + question + '"]:checked');
-      if (userAnswer && userAnswer.value === answers[question]) {
-        correctAnswers++;
-      }
+document.addEventListener('change', function() {
+  // 1: Dung, 2: Sai
+  for (let i = 1; i <= 10; i++) {
+    let selectedValue = document.querySelector(`input[name="Cau1.${i}"]:checked`);
+    if (selectedValue) {
+      let parts = selectedValue.id.split('.');
+      let answer = parts[parts.length - 1];
+      results[i - 1] = answer;
     }
-
-    // Hiển thị kết quả
-    resultDiv.textContent = "Bạn đã trả lời đúng " + correctAnswers + " câu.";
-    
-    // Ngăn chặn form gửi đi để tránh tải lại trang
-    return false;
   }
+  // 1: A, 2: B, 3: C, 4: D
+  for (let i = 1; i <= 10; i++) {
+    let selectedValue = document.querySelector(`input[name="Cau2.${i}"]:checked`);
+    if (selectedValue) {
+      let parts = selectedValue.id.split('.');
+      let answer = parts[parts.length - 1];
+      results[10 + i - 1] = answer;
+    }
+  }
+  // 1: A, 2: B, 3: C, 4: D
+  for (let i = 1; i <= 10; i++) {
+    let selectedValues = document.querySelectorAll(`input[id^="Cau3.${i}."]:checked`);
+    let answers = Array.from(selectedValues).map(input => {
+      let parts = input.id.split('.');
+      return parts[parts.length - 1];
+    });
+    results[20 + i - 1] = answers;
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    let textarea = document.getElementById(`Cau4.${i}`);
+    if (textarea) {
+      results[30 + i - 1] = textarea.value;
+    }
+  }
+});
+
+function submit() {
+  console.log(results);
+}
