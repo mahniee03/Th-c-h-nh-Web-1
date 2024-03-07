4ee0ca74f4afec3fd2b958753d8eee1fba9ced69
Đàm Minh Anh <113049898+mahniee03@users.noreply.github.com>
2024-03-07 11:49:26 +0700

    Update lambai.js

--- lambai.js
+++ lambai.js
@@ -7,7 +7,12 @@ function submit() {
     if (selectedValue) {
       let parts = selectedValue.id.split('.');
       let answer = parts[parts.length - 1];
-      results[i - 1] = answer;
+      if (answer === '1') {
+        results[i - 1] = true;
+      }
+      else {
+        results[i - 1] = false;
+      }
     }
   }
   // 1: A, 2: B, 3: C, 4: D
@@ -16,7 +21,18 @@ function submit() {
     if (selectedValue) {
       let parts = selectedValue.id.split('.');
       let answer = parts[parts.length - 1];
-      results[10 + i - 1] = answer;
+      if (answer === '1') {
+        results[10 + i - 1] = 'A';
+      }
+      else if (answer === '2') {
+        results[10 + i - 1] = 'B';
+      }
+      else if (answer === '3') {
+        results[10 + i - 1] = 'C';
+      }
+      else {
+        results[10 + i - 1] = 'D';
+      }
     }
   }
   // 1: A, 2: B, 3: C, 4: D
@@ -24,7 +40,19 @@ function submit() {
     let selectedValues = document.querySelectorAll(`input[id^="Cau3.${i}."]:checked`);
     let answers = Array.from(selectedValues).map(input => {
       let parts = input.id.split('.');
-      return parts[parts.length - 1];
+      let answer = parts[parts.length - 1];
+      if (answer === '1') {
+        return 'A';
+      }
+      else if (answer === '2') {
+        return 'B';
+      }
+      else if (answer === '3') {
+        return 'C';
+      }
+      else {
+        return 'D';
+      }
     });
     results[20 + i - 1] = answers;
   }
